7c65acdd162561b89a4d4c88fc5a8c67618d95c6
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-02-27 12:31:21 +0700

    :white_check_mark: create class and customerDto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCustomerDto {}
+import {
+  IsNotEmpty,
+  Length,
+  IsNumber,
+  IsPhoneNumber,
+  IsPositive,
+} from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNotEmpty()
+  @Length(3, 100)
+  name: string;
+  @IsNotEmpty()
+  @IsPhoneNumber('TH')
+  tel: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  point: number;
+}
--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,1 +1,14 @@
-export class Customer {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Customer {
+  @PrimaryGeneratedColumn({ name: 'customer_id' })
+  id: number;
+  @Column({ name: 'customer_name' })
+  name: string;
+  @Column({ name: 'customer_tel' })
+  tel: string;
+  @Column({ name: 'customer_point' })
+  point: number;
+  @Column({ name: 'customer_start_date' })
+  createedDate: Date;
+}
