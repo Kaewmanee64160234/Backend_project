05d73e3182f3d98e3105b4132f18094b049c4041
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-02-27 11:26:05 +0700

    :white_check_mark: create db sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CatagoriesModule } from './catagories/catagories.module';
 import { CustomersModule } from './customers/customers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './customers/entities/customer.entity';
+import { Product } from './products/entities/product.entity';
+import { Catagory } from './catagories/entities/catagory.entity';
 
 @Module({
-  imports: [ProductsModule, CatagoriesModule, CustomersModule],
+  imports: [
+    ProductsModule,
+    CatagoriesModule,
+    CustomersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Customer, Product, Catagory],
+      synchronize: true,
+      logging: false,
+      subscribers: [],
+      migrations: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
