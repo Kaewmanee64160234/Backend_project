6a75be94686a9d08e8f31bd7e2cfacb1f4269783
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-02-27 11:10:03 +0700

    stores entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CatagoriesModule } from './catagories/catagories.module';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [ProductsModule, CatagoriesModule],
+  imports: [ProductsModule, CatagoriesModule, StoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
