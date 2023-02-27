fa99b3e37b52a0bedba0295ed1b7a7fce4a8fe5e
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-02-27 12:37:17 +0700

    :construction: fix class customer

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,4 +1,10 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class Customer {
   @PrimaryGeneratedColumn({ name: 'customer_id' })
@@ -9,6 +15,10 @@ export class Customer {
   tel: string;
   @Column({ name: 'customer_point' })
   point: number;
-  @Column({ name: 'customer_start_date' })
-  createedDate: Date;
+  @CreateDateColumn({ name: 'customer_start_date' })
+  createdDate: Date;
+  @UpdateDateColumn({ name: 'customer_update_date' })
+  updatedDate: Date;
+  @DeleteDateColumn({ name: 'customer_delete_date' })
+  deletedDate: Date;
 }
