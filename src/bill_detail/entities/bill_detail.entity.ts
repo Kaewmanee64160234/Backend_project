2ec3c80273529b768f8d94ffd9554e43ae24eb8d
Manita Intharachot <64160144@go.buu.ac.th>
2023-03-09 16:47:34 +0700

    :white_check_mark:entity bill_detail

--- src/bill_detail/entities/bill_detail.entity.ts
+++ src/bill_detail/entities/bill_detail.entity.ts
@@ -1,1 +1,19 @@
-export class BillDetail {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class BillDetail {
+  @PrimaryGeneratedColumn({ name: 'bill_detail_id' })
+  id: number;
+
+  @Column({ name: 'bill_detail_name' })
+  name: string;
+
+  @Column({ name: 'bill_detail_amount' })
+  amount: number;
+
+  @Column({ type: 'float', name: 'bill_detail_price' })
+  price: number;
+
+  @Column({ name: 'bill_detail_total' })
+  total: number;
+}
