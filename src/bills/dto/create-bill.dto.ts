9dcc2744628cfc3ed229f612ebc63f2c588b2ec3
Manita Intharachot <64160144@go.buu.ac.th>
2023-03-09 15:58:05 +0700

    :memo:fix commit

--- src/bills/dto/create-bill.dto.ts
+++ src/bills/dto/create-bill.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBillDto {
   @IsNotEmpty()
+  @IsString()
   shop_name: string;
 
   @IsNotEmpty()
