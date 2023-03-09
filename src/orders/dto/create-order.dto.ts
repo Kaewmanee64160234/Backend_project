945ea2c894ccd391bc26347f14f5aa8814965793
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-03-09 15:04:27 +0700

    createOrder

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,13 @@
-export class CreateOrderDto {}
+import { IsNotEmpty, Length, IsNumber, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @Length(3, 32)
+  name: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  image: 'no_img_available.jpg';
+}
