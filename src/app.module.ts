1e15f396cdf3b90ab4e0232afd6851219257eb99
Manita Intharachot <64160144@go.buu.ac.th>
2023-03-09 14:55:09 +0700

    :white_check_mark:create bill

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { Employee } from './employees/entities/employee.entity';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { BillsModule } from './bills/bills.module';
 
 @Module({
   imports: [
@@ -51,6 +52,7 @@ import { AuthModule } from './auth/auth.module';
     MaterialsModule,
     EmployeesModule,
     AuthModule,
+    BillsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/bills/entities/bill.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Bill {
+  @PrimaryGeneratedColumn({ name: 'bill_id' })
+  id: number;
+
+  @Column({ name: 'bill_shop_name' })
+  shop_name: string;
+
+  @Column({ type: 'datetime', name: 'bill_date' })
+  date: Date;
+
+  @Column({ type: 'time', name: 'bill_time' })
+  time: Date;
+
+  @Column({ type: 'float', name: 'bill_total' })
+  total: number;
+
+  @Column({ type: 'float', name: 'bill_buy' })
+  buy: number;
+
+  @Column({ name: 'bill_change' })
+  change: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
