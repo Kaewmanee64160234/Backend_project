dd4aeff9dc405fd79ffab8d81ddd4b9b3e17bbcf
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-03-17 21:08:56 +0700

    edit findCustomerByTel

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { Customer } from './entities/customer.entity';
@@ -8,6 +8,7 @@ import { Customer } from './entities/customer.entity';
 @Injectable()
 export class CustomersService {
   constructor(
+    @InjectDataSource() private dataSource: DataSource,
     @InjectRepository(Customer)
     private readonly customersRepositiry: Repository<Customer>,
   ) {}
@@ -77,12 +78,26 @@ export class CustomersService {
 
     return this.customersRepositiry.save(customerUpdated);
   }
-  findCustomerByTel(tel: string) {
+  async findCustomerByTel(tel: string) {
     try {
-      const customer = this.customersRepositiry.find({
-        where: { tel: tel },
-      });
-      return customer;
+      const customers = await this.dataSource.query(
+        'SELECT * FROM customer WHERE customer_tel LIKE ?',
+        [`%${tel}%`],
+      );
+      const customerList = new Array<Customer>();
+      for (let i = 0; i < customers.length; i++) {
+        const customer = new Customer();
+        customer.id = customers[i].customer_id;
+        customer.name = customers[i].customer_name;
+        customer.tel = customers[i].customer_tel;
+        customer.point = customers[i].customer_point;
+        customer.image = customers[i].image;
+        customer.createdDate = customers[i].created_date;
+        customer.updatedDate = customers[i].updated_date;
+        customer.deletedDate = customers[i].deleted_date;
+        customerList.push(customer);
+      }
+      return customerList;
     } catch (errr) {
       console.log(errr);
     }
