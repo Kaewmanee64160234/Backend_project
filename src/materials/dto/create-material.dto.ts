2870c62f48da0841a34db68ab5a4b8d06e2b1649
Manita Intharachot <64160144@go.buu.ac.th>
2023-04-05 13:17:45 +0700

    update commit

--- src/materials/dto/create-material.dto.ts
+++ src/materials/dto/create-material.dto.ts
@@ -6,23 +6,16 @@ export class CreateMaterialDto {
   @IsString()
   @Length(2, 100)
   name: string;
+
   @IsNotEmpty()
-  @IsNumber()
-  @Min(5)
   min_quantity: number;
 
   @IsNotEmpty()
-  @IsNumber()
-  @Min(5)
   quantity: number;
 
   @IsNotEmpty()
   unit: string;
 
   @IsNotEmpty()
-  @Min(0)
   price_per_unit: number;
-
-  @IsNotEmpty()
-  bill_detail: BillDetail[];
 }
